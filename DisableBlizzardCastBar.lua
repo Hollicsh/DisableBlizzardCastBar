040726a53df2bb64f97464ba11e9ef368a9d3451
Will0w7 <guillermin97@gmail.com>
2025-03-01 04:49:50 +0100

    little fix

--- DisableBlizzardCastBar.lua
+++ DisableBlizzardCastBar.lua
@@ -3,7 +3,7 @@ disableCastBar:RegisterEvent("PLAYER_ENTERING_WORLD")
 disableCastBar:SetScript("OnEvent", function()
     if WOW_PROJECT_ID == WOW_PROJECT_MAINLINE then
         PlayerCastingBarFrame:UnregisterAllEvents()
-    elseif WOW_PROJECT_ID == WOW_PROJECT_CLASSIC or WOW_PROJECT_ID == WOW_PROJECT_CATACLYSM_CLASSIC then
+    else
         CastingBarFrame:UnregisterAllEvents()
     end
     disableCastBar:UnregisterEvent("PLAYER_ENTERING_WORLD")
